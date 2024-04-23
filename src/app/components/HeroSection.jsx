90496c9b45d896fd723b37e37f2e979873fbcd04
DakotaDT <dakotadt2@hotmail.com>
2024-04-23 14:43:27 -0300

    portfolio 2

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,8 +1,8 @@
 "use client";
-import React from 'react'
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation'
-import { motion } from 'framer-motion';
+import React from "react"
+import Image from "next/image"
+import { TypeAnimation } from "react-type-animation"
+import { motion } from "framer-motion";
 
 export const HeroSection = () => {
   return( 
